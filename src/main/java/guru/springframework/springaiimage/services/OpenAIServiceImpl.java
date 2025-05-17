b0b672a228962b38a198992eccaf5ffbb52e13d3
Konstantinos Angelopoulos <konangelop@gmail.com>
2025-05-17 17:14:01 +0200

    Add REST endpoint that gets as input the descpription of an image and return the AI generated image

--- src/main/java/guru/springframework/springaiimage/services/OpenAIServiceImpl.java
+++ src/main/java/guru/springframework/springaiimage/services/OpenAIServiceImpl.java
@@ -2,7 +2,24 @@ package guru.springframework.springaiimage.services;
 
 import guru.springframework.springaiimage.model.Question;
 import lombok.RequiredArgsConstructor;
+import org.springframework.ai.chat.messages.Message;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.prompt.SystemPromptTemplate;
+import org.springframework.ai.image.ImageModel;
+import org.springframework.ai.image.ImagePrompt;
+import org.springframework.ai.model.Media;
+import org.springframework.ai.openai.OpenAiChatOptions;
+import org.springframework.ai.openai.OpenAiImageOptions;
+import org.springframework.ai.openai.api.OpenAiApi;
 import org.springframework.stereotype.Service;
+import org.springframework.util.MimeTypeUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Base64;
+import java.util.List;
 
 /**
  * Created by jt, Spring Framework Guru.
@@ -11,9 +28,40 @@ import org.springframework.stereotype.Service;
 @Service
 public class OpenAIServiceImpl implements OpenAIService {
 
-    @Override
-    public Object getImage(Question question) {
+    private final ImageModel imageModel;
 
-       return new Object();
+    private final ChatModel chatModel;
+
+    @Override
+    public String getDescription(MultipartFile file) {
+
+        OpenAiChatOptions options = OpenAiChatOptions.builder()
+                .model(OpenAiApi.ChatModel.GPT_4_O.getValue())
+                .build();
+
+        var userMessage = new UserMessage("Explain what do you see in this picture?",
+                List.of(new Media(MimeTypeUtils.IMAGE_JPEG, file.getResource())));
+        Message systemMessage = new SystemPromptTemplate("If you see people in the image I want you to try to identify their gender. Refer to them as he or she.").createMessage();
+        ChatResponse response = chatModel.call(new Prompt(List.of(userMessage,systemMessage), options));
+
+        return response.getResult().getOutput().toString();
+    }
+
+    @Override
+    public byte[] getImage(Question question) {
+
+        var options = OpenAiImageOptions.builder()
+                .withHeight(1024).withWidth(1792)
+                .withResponseFormat("b64_json")
+                .withModel("dall-e-3")
+                .withQuality("hd") //default standard
+                .withStyle("natural") //default vivid
+                .build();
+
+        ImagePrompt imagePrompt = new ImagePrompt(question.question(), options);
+
+        var imageResponse = imageModel.call(imagePrompt);
+
+        return Base64.getDecoder().decode(imageResponse.getResult().getOutput().getB64Json());
     }
 }
